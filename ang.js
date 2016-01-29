9d80d904f5f516049ae34fe4b3fb6940c777229c
crsoystr <nicmetz@jackmoves.com>
2016-01-28 20:41:34 -0700

    Up to creating new comments

--- ang.js
+++ ang.js
@@ -1,9 +1,48 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+    $stateProvider
+      .state('posts', {
+    url: '/posts/{id}',
+    templateUrl: '/posts.html',
+    controller: 'PostsCtrl'
+  });
+  $urlRouterProvider.otherwise('home');
+}]);
+
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
+}]);
+
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}])
 
 app.controller('MainCtrl', [
-'$scope',
-function($scope){
+  '$scope',
+  'posts',
+  function($scope, posts){
   $scope.test = 'Hello world!';
+
+  $scope.posts = posts.posts;
 
   $scope.posts = [
     {title: 'post 1', upvotes: 5},
@@ -19,7 +58,16 @@ function($scope){
 
   $scope.addPost = function(){
   if(!$scope.title || $scope.title === '') { return; }
-  $scope.posts.push({title: $scope.title, upvotes: 0});
+  $scope.posts.push({
+  title: $scope.title,
+  link: $scope.link,
+  upvotes: 0,
+  comments: [
+    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+  ]
+});
   $scope.title = '';
+  $scope.link = '';
 };
 }]);
